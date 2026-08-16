bed815be3e1c7a218247073ecaef43e1296e6fda
AxodusYT <vishalsharma113717@gmail.com>
2026-08-17 03:41:59 +0530

    Fixed Deployement issue in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import AppRoutes from "./routes/AppRoutes";
-import Navbar from "./components/NAvbar";
+
 
 const App = () => {
   return (
     <div>
-      {/* <Navbar/> */}
+  
       <AppRoutes/>
     </div>
   );
